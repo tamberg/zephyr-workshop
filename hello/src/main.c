a030836f98bbd16ffd0b0886180c21b60e2a51b1
tamberg <tamberg@yaler.net>
2022-06-22 00:54:54 +0200

    Add files.

---
+++ hello/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/zephyr.h>
+
+void main(void) {
+    while (1) {
+        printk("Hello World? %s\n", CONFIG_BOARD);
+        k_msleep(500);
+    }
+}
