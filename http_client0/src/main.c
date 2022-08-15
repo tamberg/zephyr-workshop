93401bdcfba6c148d6649a49feacc69993051f7b
tamberg <tamberg@yaler.net>
2022-08-15 11:17:56 +0200

    Update.

--- http_client0/src/main.c
+++ http_client0/src/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+//#include <stdio.h>
 //#include <stddef.h>
 //#include <unistd.h>
 //#include <sys/socket.h>
@@ -6,10 +6,14 @@
 //#include <arpa/inet.h>
 //#include <string.h>
 
+// POSIX_C_LANG_SUPPORT // strlen()
 // CONFIG_NET_SOCKETS // zsock_...
 // CONFIG_NET_SOCKETS_POSIX_NAMES
 
 #include <zephyr/zephyr.h>
+//#include <zephyr/kernel.h>
+//#include <zephyr/logging/log.h>
+//LOG_MODULE_REGISTER(net_http_client_sample, LOG_LEVEL_DBG);
 #include <zephyr/net/socket.h>
 #include <zephyr/net/net_ip.h>
 
@@ -25,11 +29,11 @@ int main() {
 
     zsock_connect(sock_fd, (struct sockaddr *) &sv_addr_in, sizeof(struct sockaddr_in)); // connect socket to sv_addr
 
-    printf("client: writing request...\n");
+    printk("client: writing request...\n");
     char *http_req = "GET /hello.html HTTP/1.1\r\nHost: tmb.gr\r\n\r\n";
     zsock_send(sock_fd, http_req, strlen(http_req), 0);
 
-    printf("client: reading response...\n");
+    printk("client: reading response...\n");
     int n = 1024;
     char buf[n];
     int r = zsock_recv(sock_fd, buf, n, 0);
